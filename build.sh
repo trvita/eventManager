3be2a014267abf5f26d92801a26c380ca5f0feee
Likhobabina Lyudmila <trvita@github.com>
2024-02-14 00:47:22 +0700

    Add new methods without realization

--- build.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-DIRS=$(find . -type d | grep "./cmd//*" | cut -f 3 -d /)
-for DIR in $DIRS
-do 
-    go build -o build/bin/$DIR cmd/$DIR/main.go
-done
--- internal/event_server/event_server.go
+++ internal/event_server/event_server.go
@@ -7,14 +7,26 @@ import (
 )
 
 type server struct {
-	eventapi.UnimplementedGreeterServer
+	eventapi.UnimplementedEventServer
 }
 
-func MakeNewEventServer() eventapi.GreeterServer {
+func MakeNewEventServer() eventapi.EventServer {
 	return &server{}
 }
 
-func (s *server) SayHello(ctx context.Context, in *eventapi.HelloRequest) (*eventapi.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &eventapi.HelloReply{Message: "Hello " + in.GetName()}, nil
+func (s *server) MakeEvent(ctx context.Context, in *eventapi.MakeEventRequest) (*eventapi.MakeEventResponse, error) {
+	log.Printf("Received: %v", in)
+	return &eventapi.MakeEventResponse{}, nil
+}
+func (s *server) GetEvent(ctx context.Context, in *eventapi.GetEventRequest) (*eventapi.GetEventResponse, error) {
+	log.Printf("Received: %v", in)
+	return &eventapi.GetEventResponse{}, nil
+}
+func (s *server) DeleteEvent(ctx context.Context, in *eventapi.GetEventRequest) (*eventapi.DeleteEventResponse, error) {
+	log.Printf("Received: %v", in)
+	return &eventapi.DeleteEventResponse{}, nil
+}
+func (s *server) GetEvents(*eventapi.GetEventsRequest, eventapi.Event_GetEventsServer) error {
+	log.Printf("Received: %v", "GetEvents")
+	return nil
 }
