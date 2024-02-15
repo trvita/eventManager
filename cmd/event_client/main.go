7d537acde8b490eb1392d9c3bc73169e1234e30e
Likhobabina Lyudmila <trvita@github.com>
2024-02-15 22:36:10 +0700

    Add interactivity

--- cmd/event_client/main.go
+++ cmd/event_client/main.go
@@ -20,11 +20,18 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"event/api/eventapi"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -33,6 +40,7 @@ import (
 var (
 	addr = flag.String("dst", "localhost", "The server IP address")
 	port = flag.Int("p", 50051, "The server port")
+	//sdid = flag.Int64("sender-id", 0, "Sender ID")
 )
 
 func main() {
@@ -42,14 +50,124 @@ func main() {
 		log.Fatalf("Failed to connect to server: %v", err)
 	}
 	defer conn.Close()
-	c := eventapi.NewEventClient(conn)
-	r, err := c.MakeEvent(context.Background(), &eventapi.MakeEventRequest{
-		SenderID: 123,
-		Time:     20,
-		Name:     "some event",
-	})
-	if err != nil {
-		log.Fatalf("could not make event: %v", err)
+	c := eventapi.NewEventManagerClient(conn)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	// go func() {
+	// 	<-interrupt
+	// 	log.Println("Interrupt signal received. Exiting...")
+	// 	cancel()
+	// }()
+	for {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		command := strings.Fields(scanner.Text())
+
+		switch strings.ToLower(command[0]) {
+		case "makeevent":
+			senderID, err := strconv.ParseInt(command[1], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing senderID: %v", err)
+			}
+			time, err := strconv.ParseInt(command[2], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing event time: %v", err)
+			}
+			r, err := c.MakeEvent(context.Background(), &eventapi.MakeEventRequest{
+				SenderID: senderID,
+				Time:     time,
+				Name:     command[3],
+			})
+			if err != nil {
+				log.Fatalf("Could not make event: %v", err)
+			}
+			log.Printf("Created{Event ID: %d}", r.GetEventID())
+
+		case "getevent":
+			senderID, err := strconv.ParseInt(command[1], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing senderID: %v", err)
+			}
+			eventID, err := strconv.ParseInt(command[2], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing eventID: %v", err)
+			}
+			r, err := c.GetEvent(ctx, &eventapi.GetEventRequest{
+				SenderID: senderID,
+				EventID:  eventID,
+			})
+			if err != nil {
+				log.Printf("Error getting event: %v", err)
+			} else if r == nil {
+				fmt.Println("Event not found")
+			} else {
+				fmt.Printf("Event{sender_id:%d, eventId:%d, time:%d, name:%s}\n",
+					r.Event.SenderID, r.Event.EventID, r.Event.Time, r.Event.Name)
+			}
+
+		case "deleteevent":
+			senderID, err := strconv.ParseInt(command[1], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing senderID: %v", err)
+			}
+			eventID, err := strconv.ParseInt(command[2], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing eventID: %v", err)
+			}
+			r, err := c.DeleteEvent(ctx, &eventapi.GetEventRequest{
+				SenderID: senderID,
+				EventID:  eventID,
+			})
+			if err != nil {
+				log.Printf("Error getting event: %v", err)
+			} else if r == nil {
+				fmt.Println("Event not found")
+			} else {
+				fmt.Printf("%s\n", r.Deleteresponse)
+			}
+
+		case "getevents":
+			senderID, err := strconv.ParseInt(command[1], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing senderID: %v", err)
+			}
+			fromtime, err := strconv.ParseInt(command[2], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing from time: %v", err)
+			}
+			totime, err := strconv.ParseInt(command[3], 10, 64)
+			if err != nil {
+				log.Fatalf("Error parsing to time: %v", err)
+			}
+			stream, err := c.GetEvents(ctx, &eventapi.GetEventsRequest{
+				SenderID: senderID,
+				Fromtime: fromtime,
+				Totime:   totime,
+			})
+			if err != nil {
+				log.Printf("Error getting events: %v", err)
+				continue
+			}
+			fmt.Printf("Events by %d from %d to %d:\n", senderID, fromtime, totime)
+			for {
+				r, err := stream.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Printf("Error receiving events: %v", err)
+					break
+				}
+				for _, event := range r.Events {
+					fmt.Printf("Event{eventId:%d, time:%d, name:%s}\n", event.EventID, event.Time, event.Name)
+				}
+			}
+		case "exit":
+			os.Exit(0)
+		default:
+			fmt.Println("Unknown command. Available commands: MakeEvent, GetEvent, DeleteEvent, GetEvents, Exit")
+		}
 	}
-	log.Printf("Event ID: %d", r.GetEventID())
 }
--- internal/event_server/event_server.go
+++ internal/event_server/event_server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"event/api/eventapi"
 	"fmt"
+	"log"
 	"time"
 
 	"google.golang.org/grpc/codes"
@@ -36,14 +37,17 @@ func (s *server) MakeEvent(ctx context.Context, in *eventapi.MakeEventRequest) (
 		s.eventsMap = make(map[int64]*eventapi.Event)
 	}
 	s.eventsMap[eventID] = event
+	log.Printf("%d made event: %d", event.SenderID, eventID)
 	return &eventapi.MakeEventResponse{EventID: eventID}, status.New(codes.OK, "").Err()
 }
 func (s *server) GetEvent(ctx context.Context, in *eventapi.GetEventRequest) (*eventapi.GetEventResponse, error) {
 	eventID := in.EventID
+	senderID := in.SenderID
 	event, exists := s.eventsMap[eventID]
-	if !exists && event.SenderID != in.SenderID {
-		return nil, status.Errorf(codes.NotFound, "Event with ID %d by user %d not found", eventID, event.SenderID)
+	if !exists || (exists && (event.SenderID != senderID)) {
+		return nil, status.Errorf(codes.NotFound, "Event with ID %d by user %d not found", eventID, senderID)
 	}
+	log.Printf("%d got event: %d", senderID, eventID)
 	return &eventapi.GetEventResponse{
 		Event: &eventapi.Event{
 			SenderID: event.SenderID,
@@ -55,30 +59,31 @@ func (s *server) GetEvent(ctx context.Context, in *eventapi.GetEventRequest) (*e
 }
 func (s *server) DeleteEvent(ctx context.Context, in *eventapi.GetEventRequest) (*eventapi.DeleteEventResponse, error) {
 	eventID := in.EventID
+	senderID := in.SenderID
 	event, exists := s.eventsMap[eventID]
-	if !exists && event.SenderID != in.SenderID {
-		return nil, status.Errorf(codes.NotFound, "Event with ID %d by user %d not found", eventID, event.SenderID)
+	if !exists || event.SenderID != senderID {
+		return nil, status.Errorf(codes.NotFound, "Event with ID %d by user %d not found", eventID, senderID)
 	}
 	delete(s.eventsMap, eventID)
-	deleteresponse := fmt.Sprintf("Event with ID %d deleted", eventID)
+	deleteresponse := fmt.Sprintf("%d deleted event: %d", senderID, eventID)
+	log.Print(deleteresponse)
 	return &eventapi.DeleteEventResponse{Deleteresponse: deleteresponse}, status.New(codes.OK, "").Err()
 }
-func (s *server) GetEvents(req *eventapi.GetEventsRequest, stream eventapi.EventManager_GetEventsServer) error {
-	fromTime := time.Unix(0, req.Fromtime)
-	toTime := time.Unix(0, req.Totime)
+func (s *server) GetEvents(in *eventapi.GetEventsRequest, stream eventapi.EventManager_GetEventsServer) error {
+	fromTime := time.Unix(0, in.Fromtime)
+	toTime := time.Unix(0, in.Totime)
 	var eventsToSend []*eventapi.Event
 
 	for _, event := range s.eventsMap {
 		eventTime := time.Unix(0, event.Time)
-		if event.SenderID == req.SenderID && eventTime.After(fromTime) && eventTime.Before(toTime) {
+		if event.SenderID == in.SenderID && eventTime.After(fromTime) && eventTime.Before(toTime) {
 			eventsToSend = append(eventsToSend, event)
 		}
 	}
 	response := &eventapi.GetEventsResponse{
-		SenderID: req.SenderID,
-		Fromtime: req.Fromtime,
-		Totime:   req.Totime,
+		SenderID: in.SenderID,
 		Events:   eventsToSend,
 	}
+	log.Printf("%d requested events", in.SenderID)
 	return stream.Send(response)
 }
